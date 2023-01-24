ffe11f699a0aaa7873c1a478f8871d85200fea82
володя вовчко <2vova1@ukr.net>
2023-01-24 22:26:36 +0200

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,52 +4,52 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-//        MyArrayList<Integer> arrayList = new MyArrayList<>(10);
-//        arrayList.add(0);
-//        arrayList.add(1);
-//        arrayList.add(3);
-//        arrayList.add(3);
-//        arrayList.add(4);
-//        arrayList.add(5);
-//        System.out.println("before remove");
-//        for (int i = 0; i < arrayList.length(); i++) {
-//            System.out.println(arrayList.get(i));
-//        }
-//        arrayList.remove(3);
-//        System.out.println("after remove");
-//        for (int i = 0; i < arrayList.length(); i++) {
-//            System.out.println(arrayList.get(i));
-//        }
-//        arrayList.clear();
-//        arrayList.show();
-//        LinkedList<Integer> linkedList = new LinkedList<>();
-//        Queue<Integer> queue;
-//        MyQueue<Integer> myQueue = new MyQueue<>();
-//        myQueue.add(0);
-//        myQueue.add(1);
-//        myQueue.add(2);
-//        myQueue.add(3);
-//        System.out.println(myQueue.peek());
-//        System.out.println(myQueue.poll());
-//        System.out.println(myQueue.peek());
-//        Stack<Integer> stack = new Stack<>();
-//        stack.push(0);
-//        stack.push(1);
-//        stack.push(2);
-//        stack.push(3);
-//        stack.push(4);
-//        System.out.println(stack.peek());
-//        MyStack<Integer> myStack = new MyStack<>();
-//        myStack.push(0);
-//        myStack.push(1);
-//        myStack.push(2);
-//        myStack.push(3);
-//        myStack.push(4);
-//        myStack.push(5);
-//        myStack.remove(1);
-//        System.out.println(myStack.peek());
-//        System.out.println(myStack.pop());
-//        System.out.println(myStack.peek());
+        MyArrayList<Integer> arrayList = new MyArrayList<>(10);
+        arrayList.add(0);
+        arrayList.add(1);
+        arrayList.add(3);
+        arrayList.add(3);
+        arrayList.add(4);
+        arrayList.add(5);
+        System.out.println("before remove");
+        for (int i = 0; i < arrayList.length(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+        arrayList.remove(3);
+        System.out.println("after remove");
+        for (int i = 0; i < arrayList.length(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+        arrayList.clear();
+        arrayList.show();
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        Queue<Integer> queue;
+        MyQueue<Integer> myQueue = new MyQueue<>();
+        myQueue.add(0);
+        myQueue.add(1);
+        myQueue.add(2);
+        myQueue.add(3);
+        System.out.println(myQueue.peek());
+        System.out.println(myQueue.poll());
+        System.out.println(myQueue.peek());
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        System.out.println(stack.peek());
+        MyStack<Integer> myStack = new MyStack<>();
+        myStack.push(0);
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        myStack.push(4);
+        myStack.push(5);
+        myStack.remove(1);
+        System.out.println(myStack.peek());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.peek());
         MyHashMap<String, String> myHashMap = new MyHashMap<>();
         myHashMap.put("Ukraine", "Kyiv");
         myHashMap.put("Poland", "Warsaw");
